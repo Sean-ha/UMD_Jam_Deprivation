bba17bfcb87f96f72ea3d7568a041bf1da285eaa
Sean <seanha2013@gmail.com>
2020-09-18 22:39:40 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float movementSpeed = 5f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("HorizontalSpeed", horizontalInput);
+        animator.SetFloat("VerticalSpeed", verticalInput);
+        if(horizontalInput != 0) 
+        { 
+            animator.SetFloat("LastVertical", 0); 
+            animator.SetFloat("LastHorizontal", horizontalInput); 
+        }
+        if(verticalInput != 0) 
+        { 
+            animator.SetFloat("LastVertical", verticalInput); 
+            animator.SetFloat("LastHorizontal", 0); 
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = new Vector2(horizontalInput * movementSpeed, verticalInput * movementSpeed);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# UMD_Jam_Deprivation
+ Game made for UMBC/UMD/AACC game jam 2020
